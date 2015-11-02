5f8fbfbcd73a96d669674e2bbee4c11e4f1e178f
João Soares <joao420soares@gmail.com>
2015-11-02 14:05:00 +0000

    syncing...

---
+++ SESDAD/Publisher/Publisher.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting;
+using SESDAD;
+
+namespace SESDADPublisher
+{
+    class Publisher
+    {
+        public string address;
+        public string brokerAddress;
+        public TcpChannel channel;
+        public RemoteBroker remoteBroker;
+        private int SequenceNumber = 0;
+
+        static void Main(string[] args)
+        {
+            Publisher pub = new Publisher(args[0], args[1]);
+            Console.WriteLine("write [publish] to create and flood event or [quit] to exit...");
+            string input = Console.ReadLine();
+            while (!input.Equals("quit"))
+            {
+                if (input.Equals("publish"))
+                {
+                    Console.WriteLine("insert: [Topic]...");
+                    string topic = Console.ReadLine();
+                    Console.WriteLine("insert: [Message]...");
+                    string message = Console.ReadLine();
+                    Event e = new Event(message, topic, pub.remoteBroker.name);
+                    pub.publishEvent(e);
+                }
+            }
+        }
+
+        public Publisher(string address, string brokerAddress)
+        {
+            this.address = address;
+            this.brokerAddress = brokerAddress;
+            channel = new TcpChannel(new Uri(address).Port);
+            ChannelServices.RegisterChannel(channel, true);
+            remoteBroker = (RemoteBroker) Activator.GetObject(typeof(RemoteBroker), brokerAddress);
+        }
+
+        public void publishEvent(Event e)
+        {
+            e.publisher = address;
+            e.SequenceNumber = ++SequenceNumber;    // 
+            remoteBroker.Advertise(e.topic, address);
+            remoteBroker.Flood(e);
+        }
+    }
+}
---
+++ SESDAD/Subscriber/Subscriber.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting;
+using SESDAD;
+
+namespace SESDADSubscriber
+{
+    class Subscriber
+    {
+        public List<string> topicList = new List<string>();
+        public RemoteBroker serviceBroker;
+        public RemoteSubsriber remoteSubscriber;
+        public string address;
+        static void Main(string[] args)
+        {
+            string input = "";
+            Subscriber subs = new Subscriber(args[0], args[1]);  // arg[0] -> susbscriber address || arg[1] -> broker address
+            while (!input.Equals("quit"))
+            {
+                Console.WriteLine(  "write [topic] you wish to Subscribe..." 
+                                    + Environment.NewLine + "write [unsubscribe] to remove subscription of events...");
+                input = Console.ReadLine();        
+                if (input.Equals("unsubscribe"))
+                {
+                    Console.WriteLine("insert [topic] to unsubscribe...");
+                    string topic = Console.ReadLine();
+                    subs.Unsubscribe(topic);
+                }
+                else
+                {
+                    subs.Subscribe(input);
+                }  
+                
+            }
+        }
+
+        public Subscriber(string address, string broker_address)
+        {
+            this.address = address;
+            TcpChannel channel = new TcpChannel(new Uri(address).Port);//Creates the tcp channel on the port given in the config file....
+            ChannelServices.RegisterChannel(channel, true);
+            remoteSubscriber = new RemoteSubsriber();
+            remoteSubscriber.OnNotifySubscription += new NotifyEvent(ShowEvent);
+            serviceBroker = (RemoteBroker) Activator.GetObject(typeof(RemoteBroker), broker_address);
+            string[] args = address.Split(':');
+            string[] portAndName = args[2].Split('/');
+            RemotingServices.Marshal(remoteSubscriber, portAndName[1]);
+        }
+        public void ShowEvent(Event e)
+        {
+            Console.WriteLine("Receiving Subscription Event..." + Environment.NewLine + e.Message());
+        }
+
+        public void Subscribe(string topic)
+        {
+            serviceBroker.Subscribe(topic, address);
+        }
+
+        public void Unsubscribe(string topic)
+        {
+            serviceBroker.UnSubscribe(topic, address);
+        }
+    }
+}
